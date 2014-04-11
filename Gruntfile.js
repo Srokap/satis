5d7ead42ad0395c17f856c1f97bfa1d8fc7e4293
Paweł Sroka <srokap@gmail.com>
2014-04-12 00:36:22 +0200

    Grunt integration

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    shell: {
+      build: {
+        command: 'php bin/satis build satis.json',
+        options: {
+          stdout: true
+        }
+      }
+    },
+    watch: {
+      files: ['satis.json'],
+      tasks: ['shell:build']
+    }
+  });
+
+  // Load the plugin that provides the "shell" task.
+  grunt.loadNpmTasks('grunt-shell');
+
+  // Load the plugin that provides the "watch" task.
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['shell']);
+
+};
